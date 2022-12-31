b60158eee0dfd6fab8f068e15ecc31514b46e473
Peter <shumatepfs@gmail.com>
2022-12-31 00:49:34 -0500

    Set correct path to file

--- osrs/settings.py
+++ osrs/settings.py
@@ -6,7 +6,7 @@ PORT = 8086
 DB_NAME = os.getenv('DB_NAME', 'osrsdb_test')
 LOG_NAME = os.getenv('LOG_NAME', 'logs.log')
 
-USER_FILE = "osrs/users.json"
+USER_FILE = "users.json"
 
 URL_API = "https://secure.runescape.com/m=hiscore_oldschool/index_lite.ws?player={}"
 URL_HTML = "https://secure.runescape.com/m=hiscore_oldschool/overall"
